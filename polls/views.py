9348f910d74036f5aa1dc4ffc94240fb03509a5f
chef <chef@chef-PC>
2016-08-23 02:20:53 +1000

    Tutorial 3 - 2, view use template with django.template.loader

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 # Create your views here.
 def index(request):
+    latest_question_list=Question.objects.order_by('-pub_date')[:5]
+    template=loader.get_template('polls/index.html')
+    context={
+        'latest_question_list': latest_question_list,            
+    }
+    return HttpResponse(template.render(context, request))
+
+
+
+def index_2(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+
+def index_1(request):
     httpString = "Hell world. You're at the polls index."
     return HttpResponse(httpString)
 
