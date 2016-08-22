043d4199c81f75b4f1b7e876ef66c1012175fc93
chef <chef@chef-PC>
2016-08-23 02:25:02 +1000

    Tutorial 3 - 3, use django.shortcuts.render to render template

--- polls/views.py
+++ polls/views.py
@@ -6,6 +6,12 @@ from .models import Question
 
 # Create your views here.
 def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+
+def index_3(request):
     latest_question_list=Question.objects.order_by('-pub_date')[:5]
     template=loader.get_template('polls/index.html')
     context={
