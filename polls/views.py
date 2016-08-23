075bb4a99e0ba48a1df6a02268cba233407314ba
chef <chef@chef-PC>
2016-08-23 11:59:54 +1000

    Tutorial 4.2, Amend views for generic views

--- polls/views.py
+++ polls/views.py
@@ -1,17 +1,39 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponse, Http404
 from django.core.urlresolvers import reverse
+from django.views import generic
+
 
 from django.template import loader
 
 from .models import Choice, Question
+
+
+# use Django's generic views
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+    
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+        
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name= 'polls/detail.html'
+    
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+    
 
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
-
 
 def index_3(request):
     latest_question_list=Question.objects.order_by('-pub_date')[:5]
